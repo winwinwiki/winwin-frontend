ac101d2310d4378dd1ba5cd2e78d32dcfda1e016
Vinil Pansare <Vinil.Pansare@Talentica.com>
2018-07-18 17:12:33 +0530

    added progress index ui

--- src/containers/orgDetail/index.js
+++ src/containers/orgDetail/index.js
@@ -3,11 +3,19 @@ import SideBar from '../sidebar';
 
 const OrgDetail = (props) => {
     return (
-    <div className="d-flex flex-column h-100">
-        <SideBar url={props.url}/>
-        <h1>Changing the way the world finds opportunities and solves problems in org detail</h1>
-        {props.children}
-    </div>
+    <React.Fragment>
+        <div className="py-4 border-bottom d-flex justify-content-between">
+            <h2>Beverly Art Centre</h2>
+            <div className="d-flex align-items-center">
+                <h6 className="font-weight-light m-0">Last update: Jens Molbak on 1 Mar’18</h6>
+                <a href="javascript:;" className="mr-1"><i className="icon-menu mr-2 ml-5"></i> Menu</a>
+            </div>
+        </div>
+        <div className="d-flex h-100">
+            <SideBar url={props.url}/>
+            {props.children}
+        </div>
+    </React.Fragment>
     )
 }
 
--- src/containers/orgDetail/orgDetailPage.js
+++ src/containers/orgDetail/orgDetailPage.js
@@ -2,71 +2,51 @@ import React from 'react';
 
 const OrgDetailPage = (props) => {
     return (
-      <section className="dashboard-content p-0">
-      <div className="d-flex py-3 align-items-center applied-filters-container">
-          <div className="result-count">
-              1,203 organizations found in detail
-          </div>
-          <div className="applied-filters col align-items-center d-flex">
-            <span className="badge badge-pill badge-secondary">Arts &amp; Culture (A20) <a href="javascript:;" className=""><i className="icon-close"></i></a></span>
-            <span className="badge badge-pill badge-secondary">Allison Zimmerman  <a href="javascript:;" className=""><i className="icon-close"></i></a></span>
-            <span className="badge badge-pill badge-secondary">Basic Human Need - Opportunity - All  <a href="javascript:;" className=""><i className="icon-close"></i></a></span>
-            <a href="javascript:;" className="">+ 2 More</a>
-        </div>
-          <div className="clear-filters">
-              <a href="javascript:;" className="">Clear All Filters</a>
-          </div>
-      </div>
-      <div>
-          <table className="table table-bordered table-hover">
-              <thead className="thead-light">
-              <tr>
-                  <th scope="col">#</th>
-                  <th scope="col" className="sortable">First</th>
-                  <th scope="col">Last</th>
-                  <th scope="col">Handle</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                  <th scope="row">
-                      <div className="custom-control custom-checkbox">
-                          <input id="customCheckCustom" type="checkbox" className="custom-control-input"/>
-                          <label htmlFor="customCheckCustom"className="custom-control-label">&nbsp;</label>
+      <section className="dashboard-content p-0 py-3 org-details-container">
+          <div className="col-md-18 m-auto card">
+              <div className="col-md-18 m-auto d-flex flex-column py-3">
+                  <h3>Organization Description</h3>
+                  <p>Arts center conducts classes on any artistic or cultural topics ranging from ?crafts, dance, singing, painting. Camps for youth and adults and events ?open to the public. They also offer open space for private events.</p>
+
+                  <div className="section-title">
+                      Data Sets
+                  </div>
+                  <form>
+                      <div className="form-group">
+                          <label htmlFor="category">Category</label>
+                          <input type="text" className="form-control" id="category" placeholder="Community Involvement Data"/>
                       </div>
-                  </th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-              </tr>
-              <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-              </tr>
-              <tr>
-                  <th scope="row">3</th>
-                  <td>Larry the Bird</td>
-                  <td>Larry the Bird 2</td>
-                  <td>@twitter</td>
-              </tr>
-              <tr>
-                  <th scope="row">4</th>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-              </tr>
-              <tr>
-                  <th scope="row">5</th>
-                  <td>Larry the Bird</td>
-                  <td>Larry the Bird 2</td>
-                  <td>@twitter</td>
-              </tr>
-              </tbody>
-          </table>
-      </div>
-  </section>
+                      <div className="form-group">
+                          <label htmlFor="description">Description</label>
+                          <textarea name="" id="description" rows="5"></textarea>
+                      </div>
+                      <div className="card custom-list-container">
+                          <div className="card-header">
+                              Opportunity
+                          </div>
+                          <div className="card-body">
+                              <h5 className="card-title">Special title treatment</h5>
+                              <ul className="">
+                                  <li>
+                                      <span>Tolerence &amp; Inclusion</span>
+                                      <ul>
+                                          <li><span>Community Safety Net</span></li>
+                                          <li><span>Religious Tolerence</span></li>
+                                      </ul>
+                                  </li>
+                                  <li>
+                                      <span>Access to Advance education</span>
+                                      <ul>
+                                          <li><span>Years of Tertiary Schooling</span></li>
+                                      </ul>
+                                  </li>
+                              </ul>
+                          </div>
+                      </div>
+                  </form>
+              </div>
+          </div>
+      </section>
     )
 }
 
