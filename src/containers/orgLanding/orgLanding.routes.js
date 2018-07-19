a90a3967c709fea997aa61894aa3ea31dc9ab907
Sumit Chaudhari <sumit@cryptorai.com>
2018-07-19 12:51:10 +0530

    private router added to autherize the access

--- src/containers/orgLanding/orgLanding.routes.js
+++ src/containers/orgLanding/orgLanding.routes.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import { Switch } from 'react-router';
+import PrivateRoute from  '../privateRouter';
 
 import OrgLanding from './';
 import OrgDetailRoutes from '../orgDetail/orgDetail.routes';
 import OrgList from './orgList';
 import CreateOrg from '../createOrg';
 
-const OrgLandingRoutes = () => (
+const OrgLandingRoutes = (props) => (
     <OrgLanding>
           <Switch>
-              <Route exact path="/organizations" component={OrgList} />
-              <Route exact path="/organizations/new" component={CreateOrg} />
-              <Route path="/organizations/:id" component={OrgDetailRoutes} />
+              <PrivateRoute authenticated={props.isAuthenticated} exact path="/organizations" component={OrgList} />
+              <PrivateRoute authenticated={props.isAuthenticated} exact path="/organizations/new" component={CreateOrg} />
+              <PrivateRoute authenticated={props.isAuthenticated} path="/organizations/:id" component={OrgDetailRoutes} />
           </Switch>
     </OrgLanding>
 )
 
-export default OrgLandingRoutes;
+const mapStateToProps = state => ({
+    isAuthenticated: state.login.isAuthenticated
+})
+
+export default connect(
+    mapStateToProps,
+    null
+)(OrgLandingRoutes);
---
+++ src/containers/privateRouter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const PrivateRoute = ({ component: Component, authenticated, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      authenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/" />
+      )
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+    authenticated: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
