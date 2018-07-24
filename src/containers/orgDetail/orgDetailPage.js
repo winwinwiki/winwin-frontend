ae4acf01b33a272ed37e4f60973afc81897b746e
Vinil Pansare <Vinil.Pansare@Talentica.com>
2018-07-24 15:59:32 +0530

    ui updates
    left sidebar
    Social Progress Index modal
    org detail view

--- src/containers/orgDetail/orgDetailPage.js
+++ src/containers/orgDetail/orgDetailPage.js
@@ -8,17 +8,38 @@ const OrgDetailPage = (props) => {
                   <h3>Organization Description</h3>
                   <p>Arts center conducts classes on any artistic or cultural topics ranging from ?crafts, dance, singing, painting. Camps for youth and adults and events ?open to the public. They also offer open space for private events.</p>
 
-                  <div className="section-title">
+                  <div className="section-title border-bottom pb-3 mb-3">
                       Data Sets
                   </div>
                   <form>
                       <div className="form-group">
                           <label htmlFor="category">Category</label>
-                          <input type="text" className="form-control" id="category" placeholder="Community Involvement Data"/>
+                          <input type="text" className="form-control" id="category" placeholder="Enter Category" value="Community Involvement Data"/>
                       </div>
                       <div className="form-group">
                           <label htmlFor="description">Description</label>
-                          <textarea name="" id="description" rows="5"></textarea>
+                          <textarea className="form-control" name="" id="description" rows="5">The Organization is dedicated to the community to provide a variety of challenging and newartistic programming through its school of the arts, dance studio, adult and children's performance programming, special events, and education</textarea>
+                      </div>
+                      <div className="form-group">
+                          <div className="row">
+                              <div className="col">
+                                  <label htmlFor="description">Type</label><br/>
+                                  <div className="btn-group btn-group-toggle mb-4" data-toggle="buttons">
+                                      <label className="btn btn-outline-secondary">
+                                          <input type="radio" name="options" id="normal" autoComplete="off"/> Open
+                                      </label>
+                                      <label className="btn btn-outline-secondary">
+                                          <input type="radio" name="options" id="high" autoComplete="off"/> Closed
+                                      </label>
+                                  </div>
+                              </div>
+                              <div className="col">
+                                  <div className="form-group">
+                                      <label htmlFor="category">URL</label>
+                                      <input type="text" className="form-control" id="category" placeholder="Website URL" value="www.xyzabc.com"/>
+                                  </div>
+                              </div>
+                          </div>
                       </div>
                       <div className="card custom-list-container">
                           <div className="card-header">
--- src/containers/sidebar/index.js
+++ src/containers/sidebar/index.js
@@ -5,13 +5,31 @@ import { Link } from 'react-router-dom';
 const SideBar = (props) => {
 
     return (
-        <div id="mySidenav" className="sidenav">
-            <a href="javascript:void(0)" className="closebtn" onclick="closeNav()">&times;</a>
-            <Link to={`${props.url.url}`}>Basic Info</Link>
-            <Link to={`${props.url.url}/data-sets`}>Data Sets</Link>
-            <Link to={`${props.url.url}/data-sets`}>Regions Served </Link>
-            <Link to={`${props.url.url}`}>Spi Tag</Link>
-            <Link to={`${props.url.url}/data-sets`}>Sdg Tag</Link>
+        <div id="mySidenav" className="sidenav d-flex flex-column">
+            <ul className="list-group list-group-flush pr-3">
+                <li className="list-group-item"><a href="javascript:;">Basic Information</a></li>
+                <li className="list-group-item"><a href="javascript:;" className="active" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Organization Details</a>
+                    <div className="collapse" id="collapseExample">
+                        <ul className="subnav">
+                            <li><Link className="active" to={`${props.url.url}`}><i></i>Basic Info</Link></li>
+                            <li><Link to={`${props.url.url}/data-sets`}><i></i>Data Sets</Link></li>
+                            <li><Link to={`${props.url.url}/data-sets`}><i></i>Regions Served </Link></li>
+                            <li><Link to={`${props.url.url}`}><i></i>Spi Tag</Link></li>
+                            <li><Link to={`${props.url.url}/data-sets`}><i></i>Sdg Tag</Link></li>
+                        </ul>
+                    </div>
+                </li>
+                <li className="list-group-item"><a href="javascript:;">Programs</a></li>
+            </ul>
+            <div className="social-footer mt-auto">
+                <ul className="d-flex flex-row justify-content-between pl-1 pr-3">
+                    <li><a href="javascript:;"><i className="icon-globe"></i></a></li>
+                    <li><a href="javascript:;"><i className="icon-facebook"></i></a></li>
+                    <li><a href="javascript:;"><i className="icon-twitter"></i></a></li>
+                    <li><a href="javascript:;"><i className="icon-linkedin"></i></a></li>
+                    <li><a href="javascript:;"><i className="icon-pinterest"></i></a></li>
+                </ul>
+            </div>
         </div>
     )
 }
