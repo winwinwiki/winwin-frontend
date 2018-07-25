adbae44b8d8ad48dc5f67bb72613344a64f6f5fc
Sumit Chaudhari <sumit@cryptorai.com>
2018-07-25 11:39:48 +0530

    view bug fixed for sdg view

--- src/containers/orgDetail/sdgTag/index.js
+++ src/containers/orgDetail/sdgTag/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import {fetchSdgTagsList} from '../../../actions/orgDetail/sdgTagsAction';
+import { fetchSdgTagsList } from '../../../actions/orgDetail/sdgTagsAction';
 
 class SdgTags extends React.Component {
     constructor(props) {
@@ -14,42 +14,44 @@ class SdgTags extends React.Component {
     }
 
     render() {
-        const {isSdgTagsSuccess, sdgTagsList} = this.props;
-        if(!isSdgTagsSuccess) { return null; }
-    return (
-        <section className="dashboard-content p-0 py-3 org-details-container">
-            <div className="col-md-18 m-auto card">
-                <div className="col-md-18 m-auto d-flex flex-column py-3">
-                    <h3>Organization Description</h3>
-                    <p>Arts center conducts classes on any artistic or cultural topics ranging from ?crafts, dance, singing, painting. Camps for youth and adults and events ?open to the public. They also offer open space for private events.</p>
+        const { isSdgTagsSuccess, sdgTagsList } = this.props;
+        if (!isSdgTagsSuccess) { return null; }
+        return (
+            <section className="dashboard-content p-0 py-3 org-details-container">
+                <div className="col-md-18 m-auto card">
+                    <div className="col-md-18 m-auto d-flex flex-column py-3">
+                        <h3>Organization Description</h3>
+                        <p>Arts center conducts classes on any artistic or cultural topics ranging from ?crafts, dance, singing, painting. Camps for youth and adults and events ?open to the public. They also offer open space for private events.</p>
 
-                    <div className="section-title">
-                        Social Progress Index Tag
+                        <div className="section-title">
+                            Social Progress Index Tag
                     </div>
-                    <form>
-                        {this.createSpiBox(sdgTagsList)}
-                    </form>
+                        <form>
+                            {this.createSpiBox(sdgTagsList)}
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
         )
     }
 
     createSpiBox(sdgTagsList) {
         return sdgTagsList.map(tags => {
-        return (<div className="card custom-list-container">
-        <div className="card-header">
-            {tags['level1']}
-        </div>
-            <div className="card-body">
-                <ul className="">
-
-                    {tags['level2'].map(tag3 => <li><span>{tag3}</span></li>)}
-
-                </ul>
-            </div>
-        </div>)
-    })
+            return (<div className="card custom-list-container">
+                <div className="card-header">
+                    {tags['level1']}
+                </div>
+                <div className="card-body">
+                    <ul className="">
+                        <li>
+                            <ul>
+                                {tags['level2'].map(tag3 => <li><span>{tag3}</span></li>)}
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>)
+        })
     }
 }
 
