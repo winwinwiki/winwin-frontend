4c246e9da84af5c0b53f8dab58cd657456dc87e5
apoorvanyati <41937822+apoorvanyati@users.noreply.github.com>
2018-08-02 12:39:20 +0530

    program detail header modified

--- src/containers/programDetail/index.js
+++ src/containers/programDetail/index.js
@@ -6,10 +6,6 @@ const ProgramDetail = (props) => {
     <React.Fragment>
         <div className="py-4 border-bottom d-flex justify-content-between">
             <h2>Classes, camps, events and location rentals</h2>
-            <div className="d-flex align-items-center">
-                <h6 className="font-weight-light m-0">Last update: Jens Molbak on 1 Mar’18</h6>
-                <a href="javascript:;" className="mr-1"><i className="icon-menu mr-2 ml-5"></i> Menu</a>
-            </div>
         </div>
         <div className="d-flex h-100">
             <SideBar url={props.url} type={'Programs'}/>
