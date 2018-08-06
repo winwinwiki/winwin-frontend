8f4f94df8df341ad90cb1bb27be5be462ff7ff69
apoorvanyati <41937822+apoorvanyati@users.noreply.github.com>
2018-08-06 18:20:13 +0530

    changed according to given mockup

--- src/containers/orgDetail/programs/index.js
+++ src/containers/orgDetail/programs/index.js
@@ -39,61 +39,68 @@ class ProgramList extends React.Component {
         }
         return (
             <section className="dashboard-content p-0 py-3 org-details-container">
-            <div className="col-md-18 m-auto card">
-                <div className="col-md-18 m-auto d-flex flex-column py-3">
-                <div className="d-flex align-content-center border-bottom py-3">
-                    <Search placeholder="Search Program" onChange={(e) =>this.getFilteredListOfPrograms(e)} />
-                    <div className="ml-auto" data-toggle="modal" data-target="#addProgramModal">
-                        <a className="btn btn-link"><i className="icon-add mr-1"></i> Add</a>
+          <div className="col-md-18 m-auto card">
+              <div className="col-md-18 m-auto d-flex flex-column py-3">
+                  <div className="row mb-4">
+                      <div className="col">
+                        <Search placeholder="Search Program" onChange={(e) =>this.getFilteredListOfPrograms(e)} />
+                      </div>
+                      <div className="col col-md-auto" data-toggle="modal" data-target="#addProgramModal">
+                          <a href="javascript:;" className="btn btn-primary">Add Program</a>
                     </div>
-                    <div className="modal progress-index-modal fade bd-example-modal-lg" id="addProgramModal" tabIndex="-1"
-                 role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div className="modal-dialog modal-xl modal-dialog-centered" role="document">
-                            <div className="modal-content">
-                                <div className="dashboard-container">
-                                    <div className="dashboard-header">
-                                        <div className="modal-header flex-column">
-                                            <div className="d-flex w-100 p-3">
-                                                <h5 className="modal-title" id="exampleModalLabel">New Program</h5>
-                                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div className="w-100 border-top">
-                                                <div className="w-100 col d-flex align-content-center py-3">
-                                                    <div className="ml-auto">
-                                                        <button type="button" className="btn btn-link"
-                                                                data-dismiss="modal">Cancel
-                                                        </button>
-                                                        <button type="button" className="btn btn-primary">Save</button>
+                  </div>
+                  <div className="modal progress-index-modal fade bd-example-modal-lg" id="addProgramModal" tabIndex="-1"
+                    role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div className="modal-dialog modal-xl modal-dialog-centered" role="document">
+                                <div className="modal-content">
+                                    <div className="dashboard-container">
+                                        <div className="dashboard-header">
+                                            <div className="modal-header flex-column">
+                                                <div className="d-flex w-100 p-3">
+                                                    <h5 className="modal-title" id="exampleModalLabel">New Program</h5>
+                                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div className="w-100 border-top">
+                                                    <div className="w-100 col d-flex align-content-center py-3">
+                                                        <div className="ml-auto">
+                                                            <button type="button" className="btn btn-link"
+                                                                    data-dismiss="modal">Cancel
+                                                            </button>
+                                                            <button type="button" className="btn btn-primary">Save</button>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div className="modal-body dashboard-content progress-index-options">
-                                        <form>
-                                            <div className="form-group">
-                                                <label htmlFor="programName">Program Name</label>
-                                                <input type="text" className="form-control" id="programName" placeholder="Enter Program Name"/>
-                                            </div>
-                                            <div className="form-group">
-                                                <label htmlFor="description">Program Description</label>
-                                                <textarea className="form-control" name="" id="description" rows="5"placeholder="Enter Program Description"></textarea>
-                                            </div>
-                                        </form>
+                                        <div className="modal-body dashboard-content progress-index-options">
+                                            <form>
+                                                <div className="form-group">
+                                                    <label htmlFor="programName">Program Name</label>
+                                                    <input type="text" className="form-control" id="programName" placeholder="Enter Program Name"/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="description">Program Description</label>
+                                                    <textarea className="form-control" name="" id="description" rows="5"placeholder="Enter Program Description"></textarea>
+                                                </div>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <ul className="list-group py-3">
-                    {this.renderProgramList()}
-                </ul>
-            </div>
-            </div>
-        </section>
+                  <div className="row">
+                      <div className="col">
+                          <div className="list-group">
+                            {this.renderProgramList()}
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
+            
         )
     }
 
@@ -102,7 +109,7 @@ class ProgramList extends React.Component {
     }
 
     renderProgramList(){
-        return this.state.filteredProgramList.map(program =><li className="list-group-item" key={program.id}><Link to={`${this.props.match.url}/${program.id}`}>{program.name}</Link></li>);
+        return this.state.filteredProgramList.map(program =><Link to={`${this.props.match.url}/${program.id}`} className="list-group-item list-group-item-action">{program.name}</Link>);
     }
 
     getFilteredListOfPrograms(e){
