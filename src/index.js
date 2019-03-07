5db0ab7d792c80dfbf7bfff8f07c67518b1017a7
akhilesh-94 <akhilesh.shahare@talentica.com>
2019-03-07 18:57:14 +0530

    [AS_WW-02] Intergrated AWS Cognito with login

--- src/index.js
+++ src/index.js
@@ -1,59 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AppRoutes from './containers/app/app';
-import registerServiceWorker from './registerServiceWorker';
-import { ConnectedRouter } from 'react-router-redux';
-import { routerMiddleware } from 'react-router-redux'
-import createHistory from 'history/createBrowserHistory'
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
-import reducer from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import AppRoutes from "./containers/app/app";
+import registerServiceWorker from "./registerServiceWorker";
+import { ConnectedRouter } from "react-router-redux";
+import { routerMiddleware } from "react-router-redux";
+import createHistory from "history/createBrowserHistory";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import Amplify from "aws-amplify";
+import apiConfig from "./buildConfig/apiConfig";
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: apiConfig.cognito.REGION,
+    userPoolId: apiConfig.cognito.USER_POOL_ID,
+    identityPoolId: apiConfig.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: apiConfig.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+    region: apiConfig.s3.REGION,
+    bucket: apiConfig.s3.BUCKET,
+    identityPoolId: apiConfig.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "winwin",
+        endpoint: apiConfig.apiGateway.URL,
+        region: apiConfig.apiGateway.REGION
+      }
+    ]
+  }
+});
 
 export const history = createHistory();
 
 const initialState = {};
 const enhancers = [];
-const middleware = [
-    thunk,
-    routerMiddleware(history)
-  ];
+const middleware = [thunk, routerMiddleware(history)];
 
-  if (process.env.NODE_ENV === 'development') {
-    const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__
-  
-    if (typeof devToolsExtension === 'function') {
-      enhancers.push(devToolsExtension())
-    }
+if (process.env.NODE_ENV === "development") {
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+  if (typeof devToolsExtension === "function") {
+    enhancers.push(devToolsExtension());
   }
+}
 
-const store = createStore(reducer, initialState, compose(
+const store = createStore(
+  reducer,
+  initialState,
+  compose(
     applyMiddleware(...middleware),
     ...enhancers
-));
+  )
+);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <AppRoutes />
-        </ConnectedRouter>
-    </Provider>
-  , document.getElementById('root')
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <AppRoutes />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 registerServiceWorker();
 
 if (module.hot) {
-    module.hot.accept('./containers/app', () => {
-      ReactDOM.render(
-          <Provider store={store}>
-          <ConnectedRouter history={history}>
-                <AppRoutes />
-            </ConnectedRouter>
-          </Provider>
-        , document.getElementById('root')
-      );
-    });
-  }
-  
+  module.hot.accept("./containers/app", () => {
+    ReactDOM.render(
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <AppRoutes />
+        </ConnectedRouter>
+      </Provider>,
+      document.getElementById("root")
+    );
+  });
+}
