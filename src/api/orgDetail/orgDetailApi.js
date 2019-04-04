3fb84fc52487827dde92645c25383162cca4028f
akhilesh-94 <akhilesh.shahare@talentica.com>
2019-04-04 15:09:01 +0530

    Org Chart: Ad divsions, departments Change context on click of org

--- src/api/orgDetail/orgDetailApi.js
+++ src/api/orgDetail/orgDetailApi.js
@@ -1,73 +1,80 @@
-
+import CommonUtil from "../commonUtil";
 
 export function callFetchOrgHierarchyApi(orgId, callback) {
-  callback(null, {
-    "id": 2,
-    "collapsed": false,
-    "name": "Human Health Services",
-    "location": "Washington, DC",
-    "childrenType": "Division",
-    "children": [
-      {
-        "id": 1,
-        "name": "Administration for Children & Families",
-        "location": "Washington, DC",
-        "childrenType": "Department",
-        "children": [{
-          "id": 9,
-          "name": "Administration for Children & Families",
-          "location": "Washington, DC",
-          "childrenType": "Department",
-          "children": [{
-            "id": 80,
-            "name": "Administration of Community Living",
-            "location": "Washington, DC",
-            "childrenType": "Department"
-          }]
-        },
-        {
-          "id": 10,
-          "name": "Administration of Community Living",
-          "location": "Washington, DC",
-          "childrenType": "Department"
-        }]
-      },
-      {
-        "id": 3,
-        "name": "Administration of Community Living",
-        "location": "Washington, DC",
-        "childrenType": "Department"
-      },
-      {
-        "id": 4,
-        "name": "Agency for Healthcare Research and Quality",
-        "location": "Washington, DC",
-        "childrenType": "Department"
-      },
-      {
-        "id": 5,
-        "name": "Agency for Toxic Substances & Disease Registry",
-        "location": "Washington, DC",
-        "childrenType": "Department"
-      },
-      {
-        "id": 6,
-        "name": "Administration of Aging",
-        "location": "Washington, DC",
-        "childrenType": "Department"
-      },
-      {
-        "id": 7,
-        "name": "Agency for Toxic Substances & Disease Registry",
-        "location": "Washington, DC",
-        "childrenType": "Department"
-      },
-      {
-        "id": 8,
-        "name": "Administration of Aging",
-        "location": "Washington, DC",
-        "childrenType": "Department"
-      }
-    ]
-  });
+  // callback(null, {
+  //   "id": 2,
+  //   "collapsed": false,
+  //   "name": "Human Health Services",
+  //   "location": "Washington, DC",
+  //   "childrenType": "Division",
+  //   "children": [
+  //     {
+  //       "id": 1,
+  //       "name": "Administration for Children & Families",
+  //       "location": "Washington, DC",
+  //       "childrenType": "Department",
+  //       "children": [{
+  //         "id": 9,
+  //         "name": "Administration for Children & Families",
+  //         "location": "Washington, DC",
+  //         "childrenType": "Department",
+  //         "children": [{
+  //           "id": 80,
+  //           "name": "Administration of Community Living",
+  //           "location": "Washington, DC",
+  //           "childrenType": "Department"
+  //         }]
+  //       },
+  //       {
+  //         "id": 10,
+  //         "name": "Administration of Community Living",
+  //         "location": "Washington, DC",
+  //         "childrenType": "Department"
+  //       }]
+  //     },
+  //     {
+  //       "id": 3,
+  //       "name": "Administration of Community Living",
+  //       "location": "Washington, DC",
+  //       "childrenType": "Department"
+  //     },
+  //     {
+  //       "id": 4,
+  //       "name": "Agency for Healthcare Research and Quality",
+  //       "location": "Washington, DC",
+  //       "childrenType": "Department"
+  //     },
+  //     {
+  //       "id": 5,
+  //       "name": "Agency for Toxic Substances & Disease Registry",
+  //       "location": "Washington, DC",
+  //       "childrenType": "Department"
+  //     },
+  //     {
+  //       "id": 6,
+  //       "name": "Administration of Aging",
+  //       "location": "Washington, DC",
+  //       "childrenType": "Department"
+  //     },
+  //     {
+  //       "id": 7,
+  //       "name": "Agency for Toxic Substances & Disease Registry",
+  //       "location": "Washington, DC",
+  //       "childrenType": "Department"
+  //     },
+  //     {
+  //       "id": 8,
+  //       "name": "Administration of Aging",
+  //       "location": "Washington, DC",
+  //       "childrenType": "Department"
+  //     }
+  //   ]
+  // });
+  let url = CommonUtil.createUrl(`/organization/${orgId}/suborganization`);
+  fetch(url)
+    .then(response => response.json())
+    .then(responseJson => callback(null, responseJson))
+    .catch(error => {
+      callback(error, null);
+    });
 }
--- src/reducers/orgDetail/orgChartReducer.js
+++ src/reducers/orgDetail/orgChartReducer.js
@@ -1,31 +1,63 @@
-import { SET_FETCHORGHEIRARCHY_PENDING, SET_FETCHORGHEIRARCHY_SUCCESS, SET_FECTHORGHEIRARCHY_ERROR } from '../../constants/dispatch';
-  
-  const initialState = {
-    isFetchOrgHierarchyPending: false,
-    isFetchOrgHierarchySuccess: false,
-    fetchOrgHierarchyError: null,
-    orgHierarchy: {}
-  };
-  
-  export default (state = initialState, action) => {
-    switch (action.type) {
-      case SET_FETCHORGHEIRARCHY_PENDING:
-        return Object.assign({}, state, {
-            isFetchOrgHierarchyPending: action.isFetchOrgHierarchyPending
-        });
-  
-      case SET_FETCHORGHEIRARCHY_SUCCESS:
-        return Object.assign({}, state, {
-            isFetchOrgHierarchySuccess: action.isFetchOrgHierarchySuccess,
-            orgHierarchy: action.orgHierarchy
-        });
-  
-      case SET_FECTHORGHEIRARCHY_ERROR:
-        return Object.assign({}, state, {
-            fetchOrgHierarchyError: action.fetchOrgHierarchyError
-        });
-  
-      default:
-        return state;
-    }
-  };
+import {
+  SET_FETCHORGHEIRARCHY_PENDING,
+  SET_FETCHORGHEIRARCHY_SUCCESS,
+  SET_FECTHORGHEIRARCHY_ERROR,
+  ADD_ORG_CHART_CHILD_SUCCESS,
+  RESET_ORGHIRARCHY_SUCCESS
+} from "../../constants/dispatch";
+
+const initialState = {
+  isFetchOrgHierarchyPending: false,
+  isFetchOrgHierarchySuccess: false,
+  fetchOrgHierarchyError: null,
+  orgHierarchy: {}
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_FETCHORGHEIRARCHY_PENDING:
+      return Object.assign({}, state, {
+        isFetchOrgHierarchyPending: action.isFetchOrgHierarchyPending
+      });
+
+    case SET_FETCHORGHEIRARCHY_SUCCESS:
+      return Object.assign({}, state, {
+        isFetchOrgHierarchySuccess: action.isFetchOrgHierarchySuccess,
+        orgHierarchy: action.orgHierarchy
+      });
+
+    case RESET_ORGHIRARCHY_SUCCESS:
+      return Object.assign({}, state, {
+        isFetchOrgHierarchySuccess: true,
+        orgHierarchy: initialState
+      });
+
+    case ADD_ORG_CHART_CHILD_SUCCESS:
+      return {
+        ...state,
+        isFetchOrgHierarchySuccess: action.isFetchOrgHierarchySuccess,
+        orgHierarchy: {
+          ...state.orgHierarchy,
+          response: {
+            ...state.orgHierarchy.response,
+            children: [
+              ...state.orgHierarchy.response.children,
+              action.response.response
+            ]
+          }
+        }
+      };
+    // return Object.assign({}, state, {
+    //   isFetchOrgHierarchySuccess: action.isFetchOrgHierarchySuccess,
+    //   orgHierarchy: action.orgHierarchy
+    // });
+
+    case SET_FECTHORGHEIRARCHY_ERROR:
+      return Object.assign({}, state, {
+        fetchOrgHierarchyError: action.fetchOrgHierarchyError
+      });
+
+    default:
+      return state;
+  }
+};
