fa7c221d4ac741f307a8524be6b6e69645b86de5
akhilesh-94 <akhilesh.shahare@talentica.com>
2019-04-29 19:25:10 +0530

    UI Fixes: Added document title for crumbroutes

--- src/containers/common/crumbRoute.js
+++ src/containers/common/crumbRoute.js
@@ -3,6 +3,8 @@ import { Route, Redirect } from "react-router-dom";
 import { Breadcrumb } from "react-breadcrumbs";
 import Can from "../Can";
 import { connect } from "react-redux";
+import DocumentTitle from "react-document-title";
+import { titleCase } from "../../util/util";
 
 const mapStateToProps = state => ({
   session: state.session
@@ -20,30 +22,36 @@ export default connect(mapStateToProps)(
             search: includeSearch ? routeProps.location.search : null
           }}
         >
-          {Component && props.perform ? (
-            <Can
-              role={props.session.user && props.session.user.role}
-              perform={props.perform}
-              yes={() => (
-                <Component
-                  title={props.title}
-                  orgName={props.orgName}
-                  description={props.description}
-                  {...routeProps}
-                />
-              )}
-              no={() => <Redirect to="/organizations" />} //redirect to orglanding page on redirect
-              // no={() => <h1>crumbnooo</h1>}
-            />
-          ) : !props.perform ? (
-            <Component
-              title={props.title}
-              description={props.description}
-              {...routeProps}
-            />
-          ) : (
-            render(routeProps)
-          )}
+          <DocumentTitle
+            title={
+              props.title ? titleCase(props.title) + " - WinWin" : "WinWin"
+            }
+          >
+            {Component && props.perform ? (
+              <Can
+                role={props.session.user && props.session.user.role}
+                perform={props.perform}
+                yes={() => (
+                  <Component
+                    title={props.title}
+                    orgName={props.orgName}
+                    description={props.description}
+                    {...routeProps}
+                  />
+                )}
+                no={() => <Redirect to="/organizations" />} //redirect to orglanding page on redirect
+                // no={() => <h1>crumbnooo</h1>}
+              />
+            ) : !props.perform ? (
+              <Component
+                title={props.title}
+                description={props.description}
+                {...routeProps}
+              />
+            ) : (
+              render(routeProps)
+            )}
+          </DocumentTitle>
         </Breadcrumb>
       )}
     />
