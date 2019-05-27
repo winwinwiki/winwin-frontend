7edc19d7f1bbb5d57327ae0db661ba6657ceae3e
akhilesh-94 <akhilesh.shahare@talentica.com>
2019-05-27 13:13:07 +0530

    Bulk Create Org implemented

--- src/containers/uploadDataFeed/index.js
+++ src/containers/uploadDataFeed/index.js
@@ -2,35 +2,19 @@ import React from "react";
 import { push } from "react-router-redux";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import Geosuggest from "react-geosuggest";
 
-import Dropdown from "../ui/dropdown";
 import Upload from "../ui/upload";
-import { onDataFeed } from "../../actions/dataFeed/dataFeedAction";
+import { onCreateBulkOrg as onDataFeed } from "../../actions/organization/createBulkOrgAction";
 import validate from "../../util/validation";
-const sectorsList = ["Public", "Private", "Social"];
-const fileSourceList = ["IRS", "Other"];
+import { titleCase, formatBytes } from "../../util/util";
 
 class UploadDataFeed extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      file: null,
-      sector: sectorsList[0],
-      fileSource: fileSourceList[0],
-      location: null,
-      formError: {
-        file: null,
-        location: null
-      }
-    };
-    this._geoSuggest = null;
-    this.onChange = this.onChange.bind(this);
-    this.validateField = this.validateField.bind(this);
-    this.onDataFeed = this.onDataFeed.bind(this);
-    this.onSuggestSelect = this.onSuggestSelect.bind(this);
-    this.validateLocationField = this.validateLocationField.bind(this);
-  }
+  state = {
+    file: null,
+    formError: {
+      file: null
+    }
+  };
   componentDidMount() {
     document.title = "Upload Data Feed - WinWin";
   }
@@ -39,10 +23,7 @@ class UploadDataFeed extends React.Component {
     if (nextProps.dataFeed !== dataFeed && nextProps.dataFeed.data) {
       if (!nextProps.dataFeed.error) {
         this.setState({
-          file: null,
-          sector: sectorsList[0],
-          fileSource: fileSourceList[0],
-          location: null
+          file: null
         });
         this.props.changePage();
       }
@@ -50,38 +31,16 @@ class UploadDataFeed extends React.Component {
   }
 
   render() {
-    const { sector, fileSource, formError } = this.state;
+    const { formError, file } = this.state;
     return (
       <div className="container">
         <div className="row ">
           <div className="col-sm-12 mx-auto my-3">
-            <p>Basic Information</p>
-            <hr />
-            <Geosuggest
-              ref={el => (this._geoSuggest = el)}
-              placeholder="Search State/County/City/District"
-              className="form-control position-relative"
-              initialValue=""
-              fixtures={[]}
-              onBlur={this.validateLocationField}
-              onSuggestSelect={this.onSuggestSelect}
-            />
-            {formError.location && (
-              <div className="text-danger small">{formError.location}</div>
-            )}
-            <div className="my-3">
-              <Dropdown
-                selectedItem={sector}
-                name="sector"
-                containerClass="dropdown dropdown-with-searchbox"
-                onChange={this.onDropdownChange.bind(this)}
-                items={sectorsList}
-              />
-            </div>
             <p>File Selection</p>
             <hr />
             <Upload
               type="file"
+              name="file"
               onDrop={this.onDrop}
               accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
               text=".csv or .xls files only"
@@ -90,16 +49,6 @@ class UploadDataFeed extends React.Component {
               <div className="text-danger small">{formError.file}</div>
             )}
             {this.renderSelectedFile()}
-            <p>File Source</p>
-            <div className="my-3">
-              <Dropdown
-                selectedItem={fileSource}
-                name="fileSource"
-                containerClass="dropdown dropdown-with-searchbox"
-                onChange={this.onDropdownChange.bind(this)}
-                items={fileSourceList}
-              />
-            </div>
             <button
               className="btn btn-link w-50 mt-4"
               onClick={this.props.history.goBack}
@@ -109,6 +58,7 @@ class UploadDataFeed extends React.Component {
             <button
               className="btn btn-lg btn-primary w-50 mt-4"
               onClick={this.onDataFeed}
+              disabled={!file || formError.file}
             >
               Upload
             </button>
@@ -121,14 +71,16 @@ class UploadDataFeed extends React.Component {
   renderSelectedFile = () => {
     const { file } = this.state;
     if (file) {
+      var newFile = file.get("file");
       return (
         <div className="upload-image-wrap d-flex flex-wrap mb-4">
           <div className="col-sm-22">
-            <a href={file.preview}>{file.name}</a>
+            <a href={newFile.preview}>{titleCase(newFile.name)}</a>
           </div>
           <div className="col-sm-2">
             <span className="icon-close" onClick={this.dropFile} />
           </div>
+          <div className="col">{formatBytes(newFile.size)}</div>
         </div>
       );
     } else {
@@ -137,45 +89,31 @@ class UploadDataFeed extends React.Component {
   };
 
   dropFile = () => {
-    this.setState({ file: null });
+    this.setState({ file: null, formError: { file: null } });
   };
 
   onDrop = (recievedFiles, rejectedFiles) => {
     if (rejectedFiles && rejectedFiles.length) {
-      // this.setState({ dropError: true });
+      this.validateDataFeedForm("file", rejectedFiles[0]);
     }
     if (!rejectedFiles || !rejectedFiles.length) {
-      // this.setState({ dropError: false });
+      this.validateDataFeedForm("file", recievedFiles[0]);
     }
-    Object.keys(recievedFiles).forEach(key => {
-      let reader = new FileReader();
-      reader.onload = () => {
-        this.setState({ file: recievedFiles[key] });
-        this.props.validateDataFeedForm("file", recievedFiles[key]);
-        // this.props.onFileChange(files, deleteImages);
-      };
-      reader.readAsDataURL(recievedFiles[key]);
-    });
+    const data = new FormData();
+    data.append("file", recievedFiles[0]);
+    this.setState({ file: data });
   };
 
-  onChange(e) {
+  onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
-  }
+  };
 
-  onDropdownChange(field, value) {
+  onDropdownChange = (field, value) => {
     this.setState({ [field]: value });
-  }
-
-  validateField(e) {
-    this.validateDataFeedForm(e.target.name, e.target.value);
-  }
-
-  validateLocationField(location) {
-    this.validateDataFeedForm("location", location);
-  }
+  };
 
   validateDataFeedForm = (field, value) => {
-    const { formError } = this.props;
+    const { formError } = this.state;
     if (field === "file") {
       if (!value) {
         formError.file = "File is required.";
@@ -189,32 +127,18 @@ class UploadDataFeed extends React.Component {
         return;
       }
     }
-    if (!value) {
-      formError.location = "Location is required.";
-      this.setState({ formError });
-      return;
-    }
-    formError.location = "";
-    this.setState({ formError });
-    return;
   };
 
-  onDataFeed() {
-    const { file, location, sector, fileSource } = this.state;
-    if (!file || !location) {
+  onDataFeed = () => {
+    const { file } = this.state;
+    if (!file) {
       this.validateDataFeedForm("file", file);
-      this.validateDataFeedForm("location", location);
       return;
     }
-    this.props.onDataFeed({ file, location, sector, fileSource });
-  }
-
-  onSuggestSelect(suggest) {
-    this.props.validateDataFeedForm("location", suggest);
-    this.setState({
-      location: suggest
-    });
-  }
+    this.props.onDataFeed({ file });
+    //clear file after upload
+    this.dropFile();
+  };
 }
 
 const mapStateToProps = state => ({
