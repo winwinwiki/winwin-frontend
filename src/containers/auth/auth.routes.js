5fc2faf71404f521d800063d2cf06fa06a885921
akhilesh-94 <akhilesh.shahare@talentica.com>
2019-07-17 14:37:18 +0530

    ui fix: #453

--- src/containers/auth/auth.routes.js
+++ src/containers/auth/auth.routes.js
@@ -7,15 +7,21 @@ import Login from "./login";
 import ForgetPassword from "./forgetPassword";
 import ResetPassword from "./resetPassword";
 import newUserChangePassword from "./changePassword/newUserChangePassword";
+import PrivateRoute from "../privateRouter";
 
-const AuthRoutes = () => (
+const AuthRoutes = ({ location: { state } }) => (
   <React.Fragment>
     <Auth>
       {/* <BrowserRouter> */}
       <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/forget-password" component={ForgetPassword} />
-        <Route exact path="/reset-password" component={ResetPassword} />
+        <PrivateRoute
+          authenticated={(state && state.isAuth) || false}
+          exact
+          path="/reset-password"
+          component={ResetPassword}
+        />
         <Route exact path="/verify-user" component={newUserChangePassword} />
       </Switch>
       {/* </BrowserRouter> */}
