85d25345b27ca3f868e1a2f0755a7baedc0026e1
akhilesh-94 <akhilesh.shahare@talentica.com>
2019-07-19 16:06:36 +0530

    updated template url

--- src/buildConfig/apiConfig.stag.js
+++ src/buildConfig/apiConfig.stag.js
@@ -2,5 +2,5 @@ export const apiConfig = {
   protocol: "http",
   host: "ec2-54-70-210-57.us-west-2.compute.amazonaws.com",
   bulkUploadTemplateUrl:
-    "https://s3.amazonaws.com/winwin-public-bucket-stag/admin_bulk_create_template.csv"
+    "https://winwin-public-bucket-stag.s3-us-west-2.amazonaws.com/admin_bulk_create_template.csv"
 };
